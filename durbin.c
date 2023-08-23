4458b9935249a81b4039ed3f1b42e960e8a284c1
marcoshsbosco <58055884+marcoshsbosco@users.noreply.github.com>
2023-08-23 14:46:25 -0300

    Limpeza geral do código Adicionado prints para depuração e ideias de paralelização Removido time do makefile para compilação, adicionado make run

--- durbin.c
+++ durbin.c
@@ -20,153 +20,178 @@
 /* Include benchmark-specific header. */
 #include "durbin.h"
 
+
 /* Variáveis que tínhamos no arquivo header antes de passar o tamanho do dataset como argumento de CLI */
 int N;
 int _PB_N;
 
+
 /* Mensagem de ajuda do programa (--help) */
 void help_message() {
-  printf("--- Instruções de uso ---\n");
-  printf("./durbin.out [OPÇÕES]\n");
-  printf("\n[OPÇÕES]\n");
-  printf("-d: Tamanho do dataset. Pode ser um dentre {small, medium, large}.\n");
+    printf("--- Instruções de uso ---\n");
+    printf("./durbin.out [OPÇÕES]\n");
+    printf("\n[OPÇÕES]\n");
+    printf("-d: Tamanho do dataset. Pode ser um dentre {small, medium, large}.\n");
 }
 
 /* Array initialization. */
-static
-void init_array (int n,
-		 DATA_TYPE POLYBENCH_1D(r,N,n))
-{
-  int i, j;
+static void init_array(int n, DATA_TYPE POLYBENCH_1D(r,N,n)) {
+    int i, j;
 
-  for (i = 0; i < n; i++)
-    {
-      r[i] = (n+1-i);
+    for (i = 0; i < n; i++) {
+        r[i] = (n+1-i);
     }
 }
 
-
 /* DCE code. Must scan the entire live-out data.
-   Can be used also to check the correctness of the output. */
-static
-void print_array(int n,
-		 DATA_TYPE POLYBENCH_1D(y,N,n))
+ *Can be used also to check the correctness of the output. */
+static void print_array(int n, DATA_TYPE POLYBENCH_1D(y,N,n)) {
+    int i;
 
-{
-  int i;
+    for (i = 0; i < n; i++) {
+        printf(DATA_PRINTF_MODIFIER, y[i]);
+    }
 
-  POLYBENCH_DUMP_START;
-  POLYBENCH_DUMP_BEGIN("y");
-  for (i = 0; i < n; i++) {
-    if (i % 20 == 0) fprintf (POLYBENCH_DUMP_TARGET, "\n");
-    fprintf (POLYBENCH_DUMP_TARGET, DATA_PRINTF_MODIFIER, y[i]);
-  }
-  POLYBENCH_DUMP_END("y");
-  POLYBENCH_DUMP_FINISH;
+    printf("\n");
 }
 
 
 /* Main computational kernel. The whole function will be timed,
-   including the call and return. */
-static
-void kernel_durbin(int n,
-		   DATA_TYPE POLYBENCH_1D(r,N,n),
-		   DATA_TYPE POLYBENCH_1D(y,N,n))
-{
-  printf("entramos... entramos no kernel\n");
- DATA_TYPE z[N];
- DATA_TYPE alpha;
- DATA_TYPE beta;
- DATA_TYPE sum;
+ *including the call and return. */
+static void kernel_durbin(int n,
+                          DATA_TYPE POLYBENCH_1D(r,N,n),
+                          DATA_TYPE POLYBENCH_1D(y,N,n)) {
+    DATA_TYPE z[N];
+    DATA_TYPE alpha;
+    DATA_TYPE beta;
+    DATA_TYPE sum;
+    int i, k;
 
- int i,k;
+    #pragma scop
+    y[0] = -r[0];
+    beta = SCALAR_VAL(1.0);
+    alpha = -r[0];
 
-#pragma scop
- y[0] = -r[0];
- beta = SCALAR_VAL(1.0);
- alpha = -r[0];
+    // prints para debug
+    printf("\n-- Antes de tudo --\n");
+    printf("alpha: %lf\n", alpha);
+    printf("beta: %lf\n", beta);
+    printf("sum: %lf\n", sum);
 
- for (k = 1; k < _PB_N; k++) {
-   beta = (1-alpha*alpha)*beta;
-   sum = SCALAR_VAL(0.0);
-   for (i=0; i<k; i++) {
-      sum += r[k-i-1]*y[i];
-   }
-   alpha = - (r[k] + sum)/beta;
+    printf("r: ");
+    for (int i = 0; i < N; i++) {
+        printf("%0.2lf ", r[i]);
+    }
+    printf("\n");
 
-   for (i=0; i<k; i++) {
-      z[i] = y[i] + alpha*y[k-i-1];
-   }
-   for (i=0; i<k; i++) {
-     y[i] = z[i];
-   }
-   y[k] = alpha;
- }
-#pragma endscop
+    printf("y: ");
+    for (int i = 0; i < N; i++) {
+        printf("%0.2lf ", y[i]);
+    }
+    printf("\n");
+    // fim prints para debug
 
+    for (k = 1; k < _PB_N; k++) {
+        beta = (1-alpha*alpha)*beta;
+        sum = SCALAR_VAL(0.0);
+
+        printf("\n------ k = %d ------\n", k);
+        printf("beta: %lf\n", beta);
+        printf("\n");
+
+        // fork aqui
+        for (i=0; i<k; i++) {
+            sum += r[k-i-1]*y[i];
+
+            printf("-- i = %d --\n", i);
+            printf("sum: %lf\n", sum);
+        }
+        // join aqui
+
+        alpha = - (r[k] + sum)/beta;
+        printf("\nalpha: %lf\n\n", alpha);
+
+        // fork aqui
+        for (i=0; i<k; i++) {
+            z[i] = y[i] + alpha*y[k-i-1];
+
+            printf("-- i = %d --\n", i);
+            printf("z[%d]: %0.2lf\n", i, z[i]);
+        }
+        printf("\n");
+
+        // barreira aqui
+
+        for (i=0; i<k; i++) {
+            y[i] = z[i];
+            printf("-- i = %d --\n", i);
+            printf("y[%d]: %0.2lf\n", i, y[i]);
+        }
+        printf("\n");
+        // join aqui
+
+        y[k] = alpha;
+        printf("y[%d]: %lf\n", k, y[k]);
+    }
+    #pragma endscop
 }
 
 
 int main(int argc, char** argv) {
-  /*Lidar com argumentos passados por CLI*/
-  for (int i = 0; i < argc; i++) {
+    /*Lidar com argumentos passados por CLI*/
+    for (int i = 0; i < argc; i++) {
+        /* Define tamanho da entrada */
+        if (strcmp(argv[i], "-d") == 0) {
+            i++;
 
-    /* Define tamanho da entrada */
-    if (strcmp(argv[i], "-d") == 0) {
-      i++;
+            if (strcmp(argv[i], "small") == 0) {
+                N = 4;  // 250k antes
+            } else if (strcmp(argv[i], "medium") == 0) {
+                N = 350000;
+            } else if (strcmp(argv[i], "large") == 0) {
+                N = 450000;
+            } else {
+                help_message();
 
-      if (strcmp(argv[i], "small") == 0) {
-        N = 1000;  // 250k antes
-      } else if (strcmp(argv[i], "medium") == 0) {
-        N = 350000;
-      } else if (strcmp(argv[i], "large") == 0) {
-        N = 450000;
-      } else {
-        help_message();
+                return -1;
+            }
+        } else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) { /* Informações help */
+            help_message();
 
-        return -1;
-      }
+            return 0;
+        }
     }
 
-    /* Informações help */
-    else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
-      help_message();
+    /* Retrieve problem size. */
+    int n = N;
+    _PB_N = POLYBENCH_LOOP_BOUND(N,n);
 
-      return 0;
-    }
-  }
+    /* Variable declaration/allocation. */
+    POLYBENCH_1D_ARRAY_DECL(r, DATA_TYPE, N, n);
+    POLYBENCH_1D_ARRAY_DECL(y, DATA_TYPE, N, n);
 
-  /* Retrieve problem size. */
-  int n = N;
-   _PB_N = POLYBENCH_LOOP_BOUND(N,n);
+    /* Initialize array(s). */
+    init_array (n, POLYBENCH_ARRAY(r));
 
-  /* Variable declaration/allocation. */
-  POLYBENCH_1D_ARRAY_DECL(r, DATA_TYPE, N, n);
-  POLYBENCH_1D_ARRAY_DECL(y, DATA_TYPE, N, n);
+    /* Start timer. */
+    polybench_start_instruments;
 
+    /* Run kernel. */
+    kernel_durbin (n,
+                   POLYBENCH_ARRAY(r),
+                   POLYBENCH_ARRAY(y));
 
-  /* Initialize array(s). */
-  init_array (n, POLYBENCH_ARRAY(r));
+    /* Stop and print timer. */
+    polybench_stop_instruments;
+    polybench_print_instruments;
 
-  /* Start timer. */
-  polybench_start_instruments;
+    /* Prevent dead-code elimination. All live-out data must be printed
+     *    by the function call in argument. */
+    polybench_prevent_dce(print_array(n, POLYBENCH_ARRAY(y)));
 
-  /* Run kernel. */
-  kernel_durbin (n,
-		 POLYBENCH_ARRAY(r),
-		 POLYBENCH_ARRAY(y));
+    /* Be clean. */
+    POLYBENCH_FREE_ARRAY(r);
+    POLYBENCH_FREE_ARRAY(y);
 
-  /* Stop and print timer. */
-  polybench_stop_instruments;
-  polybench_print_instruments;
-
-  /* Prevent dead-code elimination. All live-out data must be printed
-     by the function call in argument. */
-  polybench_prevent_dce(print_array(n, POLYBENCH_ARRAY(y)));
-
-  /* Be clean. */
-  POLYBENCH_FREE_ARRAY(r);
-  POLYBENCH_FREE_ARRAY(y);
-
-  return 0;
+    return 0;
 }
--- makefile
+++ makefile
@@ -2,8 +2,12 @@ make:
 	@clear
 	gcc -O3 -I ./ ./polybench.c -o durbin.out durbin.c
 	@echo Executando programa...
-	@{ time ./durbin.out -h; }
+	./durbin.out -d small
 
 clean:
 	@clear
 	rm -f *.out
+
+run:
+	@clear
+	@{ time ./durbin.out -d small; }
