b08aed7d36b4ccb596107f662d45639efbd212f9
Bosco <58055884+marcoshsbosco@users.noreply.github.com>
2023-10-04 20:02:26 -0300

    implementado modelo híbrido corrigida quantidade de threads

--- runall.py
+++ runall.py
@@ -9,13 +9,20 @@ for size in ["small", "medium", "large"]:
 print("version: pth")
 for size in ["small", "medium", "large"]:
     print(f"size: {size}")
-    for threads in range(2, 9, 2):
+    for threads in [2, 4, 8]:
         print(f"threads: {threads}")
         os.system(f"time ./durbin_pthread.out -d {size} -t {threads}")
 
 print("version: mpi")
 for size in ["small", "medium", "large"]:
     print(f"size: {size}")
-    for threads in range(2, 9, 2):
+    for threads in [2, 4, 8]:
         print(f"threads: {threads}")
         os.system(f"time mpirun --oversubscribe -n {threads} ./durbin_mpi.out -d {size}")
+
+print("version: hyb")
+for size in ["small", "medium", "large"]:
+    print(f"size: {size}")
+    for threads in [(2, 1), (2, 2), (2, 4)]:
+        print(f"threads: {threads}")
+        os.system(f"time mpirun --oversubscribe -n {threads[0]} ./durbin_mpi.out -d {size} -t {threads[1]}")
